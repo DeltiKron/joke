68424cb7f9ad49aeb30c310bc3761cd1cf229e5c
Your Name <you@example.com>
2019-04-09 12:30:15 +0200

    adding stuff

---
+++ joke/joke.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+def print_joke():
+    '''Prints a Joke
+
+    Args: 
+
+    Returns:
+      None
+    
+    Example:
+    
+    '''
+    print('- Want to drink a bottle of beer?')
+    sleep(1)
+    print('- Funny how you pronounce "case" :-)')
+          
+if __name__ == '__main__':
+          print_joke()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Bad-Joke',
+      version='1.0',
+      description='Tells a bad joke about beer',
+      author='Carl Schaffer',
+      author_email='schaffer@leibniz-kis.de',
+      packages=['joke']
+     )
